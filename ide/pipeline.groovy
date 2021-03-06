2ad5f8ccb869425bfb4673fadd709d436fa01ec7
Stephane Boro <sboro@protonmail.com>
2021-03-06 12:41:35 -0500

    copy pipeline out of jenkins web ui into a pipeline.groovy file to edit in vscode

---
+++ ide/pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git url: 'https://github.com/sboro/jgsu-spring-petclinic.git', branch: 'main'
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                sh './mvnw clean package'
+            }
+
+            post {
+                
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
